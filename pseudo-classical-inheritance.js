67fc68c241bfa098ca062d7649a10f2a3beb866b
Tim Pote <pote.timothy@gmail.com>
2012-10-08 16:48:52 -0500

    Added testing code

--- pseudo-classical-inheritance.js
+++ pseudo-classical-inheritance.js
@@ -2,8 +2,18 @@
 
 load('function-augmentation.js');
 
-Function.method('new', function() {
-  var that  = Object.create(this.prototype);
-  var other = this.apply(that, arguments);
-  return (typeof other === 'object' && other) || that;
-});
+var Mammal = function(name) {
+  this.name = name;
+};
+
+Mammal.prototype.get_name = function() {
+  return this.name;
+};
+
+Mammal.prototype.says = function() {
+  return this.saying || '';
+};
+
+var myMammal = new Mammal('Kitty!');
+
+print(myMammal.get_name());
